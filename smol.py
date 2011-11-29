2e2527bbddce7cae0760ed71bffc90a13b8744b9
huskeypm <huskeypm@localhost>
2011-11-29 22:05:14 +0000

    Got most of math correct, I hope

--- smol.py
+++ smol.py
@@ -1,4 +1,4 @@
-# 
+#
 # Simple example that imports mesh from APBS
 # NOTE:
 #	1. mesh does not yet correspond to molecule
@@ -6,11 +6,11 @@
 #
 # ToDo:
 #	1. Complete Born ion example
-#	2. Compute flux around boundary to get kon 
+#	2. Compute flux around boundary to get kon
 #
 from dolfin import *
 
-## VARIABLES 
+## VARIABLES
 active_site_absorb = 0
 bulk_conc = 1.0
 
@@ -23,7 +23,7 @@ temp_siteZ  = 0.0
 def define_activesite(mesh):
     coor = mesh.coordinates()
 
-    # find maximum of box and trace out small region 
+    # find maximum of box and trace out small region
     ztop = np.max(coor[:,2])
 
     midpoint = np.mean(coor,axis=0)
@@ -34,7 +34,7 @@ def define_activesite(mesh):
 def define_bulkboundary(mesh)
     coor = mesh.coordinates()
 
-    # find maximum of box and trace out small region 
+    # find maximum of box and trace out small region
     zbottom = np.min(coor[:,2])
 
 
@@ -52,15 +52,15 @@ def bulk_boundary(x):
     return x[2] < (zbottom + DOLFIN_EPS)
 
 def active_site_marked(x):
-  # load in boundaries 
+  # load in boundaries
 
-  # assign '4' to Dirichlet outer boundary (i think) 
+  # assign '4' to Dirichlet outer boundary (i think)
   # assign '5' to reflective BC (verify)
   # assign '1' to active site (verify)
 
   # temporary
   r = np.linalg.norm(x)
-  isOnR = r < temp_innerR + DOLFIN_EPS 
+  isOnR = r < temp_innerR + DOLFIN_EPS
   isOnSite = abs(z-temp_siteZ) < (2+ DOLFIN_EPS)
   return isOnR*isOnSite
 
@@ -69,18 +69,18 @@ def active_site_marked(x):
   return marker
 
 def molecular_boundary_marked(x):
-  # load in boundaries 
+  # load in boundaries
 
-  # assign '4' to Dirichlet outer boundary (i think) 
+  # assign '4' to Dirichlet outer boundary (i think)
   # assign '5' to reflective BC (verify)
   # assign '1' to active site (verify)
 
   # temporary
   r = np.linalg.norm(x)
-  isOnR = r < temp_innerR + DOLFIN_EPS 
+  isOnR = r < temp_innerR + DOLFIN_EPS
   isNotOnSite = abs(z-temp_siteZ) > (2+ DOLFIN_EPS)
   return isOnR*isNotOneSite
-   
+
 
 
   # check for marker
@@ -88,14 +88,14 @@ def molecular_boundary_marked(x):
   return marker
 
 def bulk_boundary_marked(x):
-  # load in boundaries 
+  # load in boundaries
 
-  # assign '4' to Dirichlet outer boundary (i think) 
+  # assign '4' to Dirichlet outer boundary (i think)
   # assign '5' to reflective BC (verify)
   # assign '1' to active site (verify)
 
   r = np.linalg.norm(x)
-  isOnOuterR = r > temp_outerR - DOLFIN_EPS 
+  isOnOuterR = r > temp_outerR - DOLFIN_EPS
   return isOnOuterR
 
 
@@ -106,25 +106,26 @@ def bulk_boundary_marked(x):
 
 ## PDE terms
 
-# W = del_x Psi * q
-# rewrite, since needs to be exponentiated (might avoid by using 'beta' term below)
+# PMF term in Smoluchowski equation
+# F = del W(r)
 # [1]	Y. Song, Y. Zhang, T. Shen, C. L. Bajaj, J. A. McCammon, and N. A. Baker, “Finite element solution of the steady-state Smoluchowski equation for rate constant calculations.,” Biophysical Journal, vol. 86, no. 4, pp. 2017–2029, Apr. 2004.
 def SmolPMF(V,psi):
     valence = Constant(2)
 
     pmf = valence * psi
-    dpmf = grad(pmf) # presumably this is differentiating wrt xyz (or see pg 309 in Dolfin manual) 
+    dpmf = grad(pmf) # presumably this is differentiating wrt xyz (or see pg 309 in Dolfin manual)
 
 
 # given a point in xyz, computes electrostatic potential due to a sphere
 # psi(r) = Q/(4 * pi e e0 (1+kappa R)) 1/r exp(-kappa(r-R))
+# See equations (1) and (2) on pg 3 in Notetaker 2011-11-10
 def SpherePotential(x):
   z = 2;
-  e = 1; # replace w real value 
+  e = 1; # replace w real value
   Q = z * e;
   ee0 = 1; # replace with real permitivities
-  kappa = 1; # replace w Debye length 
-  bigR = 1; # replace w sphere size 
+  kappa = 1; # replace w Debye length
+  bigR = 1; # replace w sphere size
   pi = 3.14;
 
   # assuming sphere is centered at 0,0,0
@@ -132,21 +133,21 @@ def SpherePotential(x):
 
   h = Expression(4 * pi*ee0*(1+kappa*bigR))*1/r*exp(-kappa(r-R),r=0.0);
   h.r = r;
-  # PKH - how do i return value 
+  # PKH - how do i return value
 
   return h
 
 # load in APBS example, which has geometry and potential
-# but no boundary 
+# but no boundary
 def MeshNoBoundaryWPotential()
-  ## load data 
+  ## load data
   # coordinates
   fileMesh  ="example/potential-0_mesh.xml.gz"
-  # electrostatic potential 
+  # electrostatic potential
   potential="example/potential-0_values.xml.gz"
   mesh = Mesh(fileMesh);
 
-  ## define boundary 
+  ## define boundary
   define_activesite(mesh)
   define_bulkboundary(mesh)
 
@@ -154,40 +155,52 @@ def MeshNoBoundaryWPotential()
   bc_molecule=NeumannBC(V,Constant(0),molecular_boundary)
   bc_bulk = DirichletBC(V, Constant(bulk_conc), bulk_boundary)
 
-  # apply file values to fuction space 
+  # apply file values to fuction space
   psi = Function(V,potential);
 
   return mesh,psi
 
 
-# boundary is given as input, here we compute potential (for sphere) 
+# boundary is given as input, here we compute potential (for sphere)
 # given input boundary. Note: normally youd want to use APBS to get
-# the electrostatic potential 
+# the electrostatic potential
 def MeshWBoundaryNoPotential()
-  ## load data 
-  fileMesh = "example/p.pqr.output.all_mesh.xml.gz" 
+  ## load data
+  fileMesh = "example/p.pqr.output.all_mesh.xml.gz"
   mesh = Mesh(fileMesh)
-  # load markers 
+  # load markers
 
-  ## define boundary 
+  ## define boundary
   bc_active = DirichletBC(V, Constant(active_site_absorb), active_site_marked)
   bc_molecule=NeumannBC(V,Constant(0),molecular_boundary_marked)
   bc_bulk = DirichletBC(V, Constant(bulk_conc), bulk_boundary_marked)
 
-  ## compute potential 
+  ## compute potential
   v= Function(V)
-  psi = SpherePotential(v) 
- 
+  psi = SpherePotential(v)
+
   return mesh, psi
 
 ## Domain
+
+#
+# PDE
+# 0 = del D [del - beta F(r)] p(r)
+# F(r) = - del U(r)
+# Eqn 3 of pg 4 in  notetaker doc
+# but, we use integration factor instead to get Eqn (4)
+# intfact = exp(-beta*pmf)
+# 0 = del . (D intfact del (1/intfact p)
+#
+
+
 # if loading from APBS
 apbs = 0
 gamer= 1
 if(apbs==1):
   mesh, psi = MeshNoBoundaryWPotential()
 
-# sphere example 
+# sphere example
 elif(gamer==1):
   mesh, psi = MeshWBoundaryNoPotential()
 
@@ -202,7 +215,7 @@ V = FunctionSpace(mesh, "CG", 1)
 # Boundaries
 bcs = [bc_active, bc_molecule,bc_bulk]
 
-# Compute dW 
+# Compute W, dW from psi
 SmolPMF(V,psi)
 
 # The solution function
@@ -212,33 +225,36 @@ u = Function(V)
 v = TestFunction(V)
 
 # The diffusion part of PDE
-beta = 1 # Johan defines this as beta = exp(-valence*psi) to arrive at 
-         # exponentiated form of PDE 
-# refer ti Zhou eqn 2,3 uin 2011
-F = beta*inner(grad(u), grad(v))*dx
+# Recasting as integration factor (see Eqn (5) in Notes)
+beta = 1/0.693 # 1/kT, kcal/mol
+intfact = exp(- beta * pmf)
+invintfact = 1/intfact;
+# Create weak-form integrand (see eqn (6) in NOtes)
+# also refer ti Zhou eqn 2,3 uin 2011
+# NOTE: this is the u that satisfies Eqn (6), not the traditional Smol eqn
+# form of the PDE
+D = 1.0 # diffusion constant
+#  (no time dependence,so only consider del u del v term)
+F = D * intfact*inner(grad(u), grad(v))*dx
 
-# The PMF part of PDE (verify) 
-beta_nrg = 0.693 # 1/kT, kcal/mol
-pmfterm = beta_nrg * u * dpmf 
-F = inner(pmfterm(u),v)*dx   # i think this is wrong 
 
 # Solve the problem
 solve(F==0, u, bcs)
 
 # Project the solution
 # Return projection of given expression *v* onto the finite element space *V*
-# e.g. 'un-exponentiate'
-# what is this (see pg 21 of Fenics manual)?
-up = project(beta*u)
+# Solved for u that satisfies Eqn 6, so obtain u we want by transformation (See Eqn (7))
+up = project(intfact*u)
 
 File("solution.pvd") << up
 plot(up, interactive=True)
 
 
-## get kon 
-# following example on pg 619 
-#boundary_flux = assemble( inner(surf_normal,Jp) ,boundary_bulk)  # verify, might need interior bondary 
-Jp = Expression(D * del u .....) ???????
+## get kon
+# See eqn (4) of Notes
+# following example on pg 619
+#boundary_flux = assemble( inner(surf_normal,Jp) ,boundary_bulk)  # verify, might need interior bondary
+Jp = Expression(D * intfact * grad(invintfact * up) )
 domain = MeshFunction("uint", mesh, 2)
 boundary_flux_term  = assemble( Jp*ds, exterior_facet_domains = domain)
 kon = boundary_flux_term / c0;
