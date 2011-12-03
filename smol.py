1cd9156aecea2c8ca50073be6451140df294bfbd
huskeypm <huskeypm@localhost>
2011-12-03 20:47:50 +0000

    almost there

--- smol.py
+++ smol.py
@@ -14,11 +14,18 @@ import numpy as np
 ## VARIABLES
 active_site_absorb = 0
 bulk_conc = 1.0
+D = 1.0 # diffusion constant
 
 # temporary
 temp_outerR = 5.0
 temp_innerR = 1.0
 temp_siteZ  = 0.0
+
+
+# markers
+outer_boundary_marker = 4 # verify 
+molecular_boundary_marker = 5 # verify 
+
 
 ## Dirichlet Boundaries
 def define_activesite(mesh):
@@ -52,58 +59,65 @@ def bulk_boundary(x):
 
     return x[2] < (zbottom + DOLFIN_EPS)
 
-def active_site_marked(x):
-  # load in boundaries
+#def active_site_marked(x):
+class DirichletActiveSite(SubDomain):
+  def inside(self,x,on_boundary):
+    # load in boundaries
 
-  # assign '4' to Dirichlet outer boundary (i think)
-  # assign '5' to reflective BC (verify)
-  # assign '1' to active site (verify)
+    # assign '4' to Dirichlet outer boundary (i think)
+    # assign '5' to reflective BC (verify)
+    # assign '1' to active site (verify)
 
-  # temporary
-  r = np.linalg.norm(x)
-  isOnR = r < temp_innerR + DOLFIN_EPS
-  z = x[2]
-  isOnSite = abs(z-temp_siteZ) < (2+ DOLFIN_EPS)
-  return isOnR*isOnSite
+    # temporary
+    r = np.linalg.norm(x)
+    isOnR = r < temp_innerR + DOLFIN_EPS
+    z = x[2]
+    isOnSite = abs(z-temp_siteZ) < (2+ DOLFIN_EPS)
+    return isOnR*isOnSite
+  
+    # check for marker
+    #marker = 0
+    #return marker
 
-  # check for marker
-  marker = 0
-  return marker
+class DirichletBulkBoundary(SubDomain):
+  def inside(self,x,on_boundary):
+    # load in boundaries
 
-def molecular_boundary_marked(x):
-  # load in boundaries
+    # assign '4' to Dirichlet outer boundary (i think)
+    # assign '5' to reflective BC (verify)
+    # assign '1' to active site (verify)
 
-  # assign '4' to Dirichlet outer boundary (i think)
-  # assign '5' to reflective BC (verify)
-  # assign '1' to active site (verify)
+    r = np.linalg.norm(x)
+    isOnOuterR = r > temp_outerR - DOLFIN_EPS
+    return isOnOuterR
 
-  # temporary
-  r = np.linalg.norm(x)
-  isOnR = r < temp_innerR + DOLFIN_EPS
-  isNotOnSite = abs(z-temp_siteZ) > (2+ DOLFIN_EPS)
-  return isOnR*isNotOneSite
-
-
-
-  # check for marker
-  marker = 0
-  return marker
-
-def bulk_boundary_marked(x):
-  # load in boundaries
-
-  # assign '4' to Dirichlet outer boundary (i think)
-  # assign '5' to reflective BC (verify)
-  # assign '1' to active site (verify)
-
-  r = np.linalg.norm(x)
-  isOnOuterR = r > temp_outerR - DOLFIN_EPS
-  return isOnOuterR
+  
+    # check for marker
+    #marker = 0
+    #return marker
 
 
-  # check for marker
-  marker = 0
-  return marker
+class NeumannMolecularBoundary(SubDomain):
+  def inside(self,x,on_boundary):
+
+    # load in boundaries
+  
+    # assign '4' to Dirichlet outer boundary (i think)
+    # assign '5' to reflective BC (verify)
+    # assign '1' to active site (verify)
+  
+    # temporary
+    r = np.linalg.norm(x)
+    isOnR = r < temp_innerR + DOLFIN_EPS
+    z = x[2]
+    isNotOnSite = abs(z-temp_siteZ) > (2+ DOLFIN_EPS)
+    return isOnR*isNotOnSite
+  
+  
+  
+    # check for marker
+    #marker = 0
+    #return marker
 
 
 ## PDE terms
@@ -115,21 +129,56 @@ def SmolPMF(V,psi):
     valence = Constant(2)
 
     pmf = valence * psi
-    dpmf = grad(pmf) # presumably this is differentiating wrt xyz (or see pg 309 in Dolfin manual)
+   
+    # Not needed, given how I formulated the PDE 
+    #dpmf = grad(pmf) # presumably this is differentiating wrt xyz (or see pg 309 in Dolfin manual)
 
-    return pmf,dpmf
+    return pmf #,dpmf
 
-def ComputeKon():
-    #Jp = Expression("D * intfact * grad(invintfact * up)")
+## get kon
+# See eqn (4) of Notes
+# following example on pg 619
+# if sphere, validate against analytical result (see 111128_todo)
+def ComputeKon(intfact,invintfact,up,V):
+    c0 = bulk_conc;
+ 
+    # ERROR: ufl.log.UFLException: Shape mismatch.
+    # grad term is not  working, but it seems like its output should be the same size as input arguments?
+    #Jp = project(D * intfact * grad(invintfact * up),V)
+    Jp = project(D * intfact * (invintfact * up),V)
 
-    domain = MeshFunction("uint", mesh, 2)
+    subdomains = MeshFunction("uint", mesh, 2) # I called this earlier, ok to do again?
 
-    #F = {}
-    # todo
-    #boundary_flux_terms  = assemble( Jp*ds,
-    #  exterior_facet_domains = domain)
-    #kon = boundary_flux_term / c0;
 
+    # ERROR: ufl.log.UFLException: Dot product requires non-scalar arguments, got arguments with ranks 0 and 1.
+    # ???
+    #boundary_flux_terms = assemble(dot(Jp, tetrahedron.n)*ds(outer_boundary_marker),
+    n = FacetNormal(mesh)
+    boundary_flux_terms = assemble(dot(Jp, n)*ds(outer_boundary_marker),
+                                exterior_facet_domains = subdomains)
+    kon = boundary_flux_term / c0;
+
+    print "My kon is %f" % (kon)
+
+    return kon 
+
+
+# TODO:
+def GetPotential():
+  # read in apbs mesh/values (johan did this)
+  meshAPBS;
+  valuesAPBS;
+
+  # read in/pass in mesh from Gamer (different node locations)
+  meshGamer; 
+  V ; # meshGamer's functionspace  
+
+  #PKH  interpolate values onto my mesh?
+  # maybe do this: http://stackoverflow.com/questions/7701669/interpolate-large-irregular-grid-onto-another-irregular-grid-in-python
+  v = scipy.interpolate(meshAPBS,valuesAPBS,meshGamer,V)
+  
+
+  1
 
 # given a point in xyz, computes electrostatic potential due to a sphere
 # psi(r) = Q/(4 * pi e e0 (1+kappa R)) 1/r exp(-kappa(r-R))
@@ -137,25 +186,23 @@ def ComputeKon():
 def SpherePotential(x):
   z = 2;
   e = 1; # replace w real value
-#  Q = z * e;
-#  ee0 = 1; # replace with real permitivities
-#  kappa = 1; # replace w Debye length
-#  bigR = 1; # replace w sphere size
-#  pi = 3.14;
+  Q = z * e;
+  ee = 1; # ee0, replace with real permitivities
+  kappa = 1; # replace w Debye length
+  bigR = 1; # replace w sphere size
+  pi = 3.14;
 
   # assuming sphere is centered at 0,0,0
-  r = np.linalg.norm(x)
+  r0 = np.linalg.norm(x)
 
-  h = Expression("Q / (4 * pi*ee0*(1+kappa*bigR))*1/r*exp(-kappa*(r-bigR))",
-                 Q = z*e,
-                 pi = 3.14,# use actual val
-                 ee0 = 1,  # replace with real permitivities
-                 kappa = 1, # replace w Debye length
-                 bigR = 1,   # replace w sphere size
-                 r=1.0      # dummy val for now
+  h = Expression("Q / (4 * pi * ee * (1 + kappa * bigR)) * 1/r * exp(-kappa * (r-bigR))",
+                 Q = Q,   
+                 pi = pi,
+                 ee = ee, 
+                 kappa = kappa,
+                 bigR = bigR,  
+                 r=r0       
                  );
-  h.r = r;
-  # PKH - how do i return value
 
   return h
 
@@ -176,14 +223,19 @@ def MeshNoBoundaryWPotential():
   define_activesite(mesh)
   define_bulkboundary(mesh)
 
-  bc_active = DirichletBC(V, Constant(active_site_absorb), active_site)
-  # SEE PG 199 of Fenics manual TODO
-  # bc_molecule=NeumannBC(V,Constant(0),molecular_boundary)
-  bc_bulk = DirichletBC(V, Constant(bulk_conc), bulk_boundary)
-  bcs = [bc_active, bc_molecule,bc_bulk]
+  bc_active = DirichletBC(V, Constant(active_site_absorb), DirichletActiveSite())
+  bc_bulk = DirichletBC(V, Constant(bulk_conc), DirichletBulkBoundary())
+  # Neumann done later 
+  bcs = [bc_active, bc_bulk]
+  
+  print "need to add subdomain for neuman marker"
+  quit()
 
   # apply file values to fuction space
   psi = Function(V,potential);
+
+  # alternatively I need to interpolate the grid from APBS 
+  # see email from Johand around 1128
 
   return mesh,psi,bcs,V
 
@@ -192,6 +244,7 @@ def MeshNoBoundaryWPotential():
 # given input boundary. Note: normally youd want to use APBS to get
 # the electrostatic potential
 def MeshWBoundaryNoPotential():
+
   ## load data
   fileMesh = "example/p.pqr.output.all_mesh.xml.gz"
   mesh = Mesh(fileMesh)
@@ -199,35 +252,50 @@ def MeshWBoundaryNoPotential():
   # Function space
   V = FunctionSpace(mesh, "CG", 1)
 
-  # load markers
 
-  ## define boundary
-  bc_active = DirichletBC(V, Constant(active_site_absorb), active_site_marked)
-  # SEE PG 199 of Fenics manual TODO
-  #   bc_molecule=NeumannBC(V,Constant(0),molecular_boundary_marked)
-  bc_molecule=bc_active # WRONG WRONG WRONG 
-  bc_bulk = DirichletBC(V, Constant(bulk_conc), bulk_boundary_marked)
-  bcs = [bc_active, bc_molecule,bc_bulk]
+  ## load markers
+  # Need to pull these from Gamer output at some point 
 
+  ## define Dirichlet boundary
+  bc_active = DirichletBC(V, Constant(active_site_absorb), DirichletActiveSite())
+  bc_bulk = DirichletBC(V, Constant(bulk_conc), DirichletBulkBoundary())
+  bcs = [bc_active, bc_bulk]
+
+  ## Define Neumann boundary
+  # PKH: not sure if this is right (added to PDE later) 
+  subdomains = MeshFunction("uint",mesh,2)
+  subdomain = NeumannMolecularBoundary()
+  subdomain.mark(subdomains,molecular_boundary_marker)
+
+  # PKH: do I need to mark BulkBoundary as well in order to use assemble call later?
+  subdomainOuter = DirichletBulkBoundary()
+  subdomainOuter.mark(subdomains,outer_boundary_marker)
+
+  haveAPBS=0
+  if(haveAPBS):
+    psi = GetPotential()
 
   ## compute potential
-  v= Function(V)
-  # TODO psi = SpherePotential(v)
-  psi = v
-  v.vector()[:]=0  # remove me
+  else:
+    # PKH best way of doing this? 
+    x = mesh.coordinates() 
+    psi = interpolate(SpherePotential(x),V)
+    # PKH: I need to check potential, since code x-plodes 
+    psi.vector()[:]=0;
 
   return mesh, psi,bcs,V
 
-def RestOfCode():
+def PDEPart():
 
     # Compute W, dW from psi
-    pmf,dpmf = SmolPMF(V,psi)
+    pmf = SmolPMF(V,psi)
 
     # The solution function
     u = Function(V)
 
     # Test function
     v = TestFunction(V)
+
 
     # The diffusion part of PDE
     # Recasting as integration factor (see Eqn (5) in Notes)
@@ -238,12 +306,20 @@ def RestOfCode():
     # also refer ti Zhou eqn 2,3 uin 2011
     # NOTE: this is the u that satisfies Eqn (6), not the traditional Smol eqn
     # form of the PDE
-    D = 1.0 # diffusion constant
     #  (no time dependence,so only consider del u del v term)
     F = D * intfact*inner(grad(u), grad(v))*dx
 
+    # apply Neumann cond 
+    f_molecular_boundary = Constant(0) # or some function...
+
+    # PKH - check w Fenics example, since it seems like ds() no longer needed?
+    # it seems like I already marked the subdomain, so this shouldnt be
+    # needed. That said, I need to see how to apply this f_mol. function
+    F += f_molecular_boundary*v*ds(molecular_boundary_marker)
+
 
     # Solve the problem
+    # ERROR:   Process 0: *** Warning: Found no facets matching domain for boundary condition.
     solve(F==0, u, bcs)
 
     # Project the solution
@@ -252,19 +328,9 @@ def RestOfCode():
     up = project(intfact*u)
 
     File("solution.pvd") << up
-    plot(up, interactive=True)
-
-
-    ## get kon
-    # See eqn (4) of Notes
-    # following example on pg 619
-    #boundary_flux = assemble( inner(surf_normal,Jp) ,boundary_bulk)  # verify, might need interior bondary
-    ComputeKon()
-
-    # if sphere, validate against analytical result (see 111128_todo)
-
-
-
+    #plot(up, interactive=True)
+ 
+    return intfact,invintfact,up,V
 
 ## Domain
 
@@ -282,8 +348,11 @@ def RestOfCode():
 # if loading from APBS
 apbs = 0
 gamer= 1
-if(apbs==1):
-  mesh, psi,bcs,V = MeshNoBoundaryWPotential()
+if(0):
+  1
+## NOT SUPPORTED YET 
+##if(apbs==1):
+##  mesh, psi,bcs,V = MeshNoBoundaryWPotential()
 
 # sphere example
 elif(gamer==1):
@@ -296,4 +365,5 @@ else:
 
 
 
-RestOfCode()
+intfact,invintfact,up,V = PDEPart()
+ComputeKon(intfact,invintfact,up,V) 
