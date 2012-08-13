Adjusted param name to give um^3 --> inverse Molar (not Molar) Applying correct units to flux conversion Verified validation for uncharged case against Song

smol.py
@@ -20,21 +20,26 @@ parms  = params()
 problem = problem()
 results = empty()
 
-# misc
+# Areas are given in Angstroms squared, if mesh prepared by gamer from molecular structure
 def CheckAreas(mesh):
     for m in np.array([1,4,5]):
       areaExpr = Constant(1.)*ds(m) 
       area = assemble(areaExpr, mesh=mesh)
-      print "Marker %d area: %f " % (m,area) 
+      print "Marker %d area: %f [A^2]" % (m,area) 
  
 ## PDE terms
 
 # PMF term in Smoluchowski equation
 # F = del W(r)
 # [1]	Y. Song, Y. Zhang, T. Shen, C. L. Bajaj, J. A. McCammon, and N. A. Baker, Finite element solution of the steady-state Smoluchowski equation for rate constant calculations.
-def ElectrostaticPMF(problem,psi,q="useparams"):
+# V = can define alternative vector function space here instead of using one in 'problem'
+def ElectrostaticPMF(problem,psi,q="useparams",V="none"):
 
-    problem.pmf = Function(problem.V)
+    if(V=="none"):
+      problem.pmf = Function(problem.V)
+    else:
+      problem.pmf = Function(V)
+
     pmf = np.zeros( psi.vector().size() ) 
 
     if(q=="useparams"):
@@ -67,26 +72,7 @@ def ElectrostaticPMF(problem,psi,q="useparams"):
     
     problem.pmf.vector()[:] = pmf
 
-   
-
-# This works
-def Test():
-
-    mesh = UnitCube(32,32,32)
-
-    # Function space
-    V = FunctionSpace(mesh, "CG", 1)
-
-    # The potential
-    psi = Function(V)
-    psi.vector()[:] = 1.0 # WRONG WRONG WRONG 
-
-    Vv = VectorFunctionSpace(mesh,"CG",1) # need Vector, not scalar function space 
-    # Need to know the spatial dimension to compute the shape of derivatives.
-    Jp = project(D*grad(psi),Vv)
- 
-
-
+    return pmf # needed for homog.py 
 
 ## get kon
 # See eqn (4) of Notes
@@ -105,17 +91,24 @@ def ComputeKon(problem,results,subdomainMarker=-1,useSolutionVector=0,solutionVe
     # don't need to reproject Jp = project(D * intfact * grad(invintfact * up),Vv)
     Jp = parms.D * grad(solutionVector) + parms.D *  parms.beta * solutionVector * grad(problem.pmf)
 
-    print  assemble(Constant(1)*ds(subdomainMarker),
-                                mesh=problem.mesh,
-                                exterior_facet_domains = problem.subdomains)
+    # Boundary flux [Ang um^2/s], since mesh is in A 
+    # see notetaker 2012-08-13
     boundary_flux_terms = assemble(dot(Jp, tetrahedron.n)*ds(subdomainMarker),
                                 exterior_facet_domains = problem.subdomains)
+    # use -1, since I believe normals are printed INTO domain
+    boundary_flux_terms *= -1 
+    #print boundary_flux_terms, "[A um^2 /s]"
+    boundary_flux_terms *= parms.Ang_to_um      
+    #print boundary_flux_terms, "[um^3 /s]"
+    boundary_flux_terms *= parms.um3_to_invM     
+    #print boundary_flux_terms, "[um^3 /s]"
+    
     kon = boundary_flux_terms / float(parms.bulk_conc);
 
     #print "Warning: using correction until i get units right. "
     #print "Based on smol for sphere w/o charge. "
     #print "Is something wrong w scaling by bulk conc?"
-    kon = kon * parms.correction
+    #kon = kon * parms.correction
     print "kon: %e [1/Ms] %e [1/M min]" % (kon, kon*60)
 
     results.kon = kon

validation.py
@@ -178,7 +178,7 @@ def ValidationChargedSphere(problem,root,useStored=1):
   r_Ang = parms.um_to_Ang * R
   kon_elec = 4 * np.pi * D * qEff * (np.exp(qEff/r_Ang)/(np.exp(qEff/r_Ang)-1)) * c
   kon_pred = chargedresult.kon*60. # 60 [s/min]
-  print "VERIFY kon_anal %e pred %e [1/M min]" % (kon_elec, kon_pred)
+  print "VERIFY kon_anal_elec %e pred %e [1/M min]" % (kon_elec, kon_pred)
 
   return chargedresult
 
@@ -187,6 +187,7 @@ def ValidationChargedSphere(problem,root,useStored=1):
 def ValidationSphere(useStored=0):
   # params 
   root = "/home/huskeypm/scratch/validation/sphere/sphere"
+  root = "./sphere"
   problem.fileMesh = root+"_mesh.xml.gz"
   problem.fileSubdomains= root+"_subdomains.xml.gz"
 
@@ -204,9 +205,19 @@ def ValidationSphere(useStored=0):
 
   
   # results 
+  # Made this to resemble Song paper. NOTE: they use an 8 A sphere and 2 A ion, thus
+  # giving an effective radius of 10 A. To reproduce their expression, uncomment #SONG below
+  # NOTE: they solve their problem on a finite domain, whereas I assume an infinite domain,
+  # hence their solution is not exactly kon=4piRD 
   R = parms.Rsphere
-  kon_analy = 4 * np.pi * R * parms.D * parms.um3_to_M
-  print "kon_anal %e pred %e " % (kon_analy, unchargedresult.kon)
+  #R = 10 * parms.Ang_to_um  
+  #parms.D = 780
+
+  #kon_analy = 4 * np.pi * R * parms.D * parms.um3_to_invM
+  # for conceptual reasons I redefine kon in terms of surface area
+  kon_analy = (4 * np.pi * R**2)/R * parms.D * parms.um3_to_invM
+  print "kon_anal %e [1/Ms] %e [1/Mmin] pred %e [1/Ms]" % (kon_analy, kon_analy*60,\
+     unchargedresult.kon)
 
   chargedresult = ValidationChargedSphere(problem,root,useStored=useStored) 
   
@@ -237,7 +248,18 @@ def ValidationSphere(useStored=0):
   return msg
 
 if __name__ == "__main__":
-  msg="smol.py run "
+  msg="""
+\nPurpose:
+  Run validation
+
+Usage:\n"""
+  msg+= __file__+" dbg/sphere/run "
+  msg+="""
+
+Notes:
+
+"""
+
 
 
 
@@ -249,6 +271,9 @@ if __name__ == "__main__":
     ValidateGatedChannel(4.)
     ValidateLinearPotential(4.)
     #ValidationTnC(useStored=1)
+  elif(sys.argv[1]=="sphere"):
+    m1 = ValidationSphere(useStored=0)
+    
   elif(sys.argv[1]=="run"):
     m1 = ValidationSphere(useStored=0)
     m2 = serca.Validation(useStored=0)