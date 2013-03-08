b9ece11750868672bdb7fc2f8aa86ab182ef8ee2
huskeypm <huskeypm@gmail.com>
2013-03-08 15:42:43 -0800

    Added discontinuous epsilon. results are a bit strange, so needs some debugging

--- poissonboltzman.py
+++ poissonboltzman.py
@@ -6,6 +6,7 @@ import numpy as np
 # Revisions
 # Todo
 # -validate all aspects of code 
+#  The sinh expression initially comes out as zero, so it never looks like it is used in the form expression 
 # -implement log function 
 
 
@@ -22,29 +23,39 @@ Notes:
 Author:
   the computational scientist formally known as pete 
 
+
+
 """
+
+
+print "WARNING: This code is no-where close to being validated (use at your own risk)"
 
 class params:
   dim = 2  # 2d mesh 
-  molRad = 0.5 # radius of molecule [A]
+  molRad = 1.5 # radius of molecule [A]
   molMarker = 2 # marker for molecular boundary 
   eps = 0.001  # epsilson for 'error'
   domRad = 5.  # radius of domain [A] (kind of, since square)
   domMarker = 3  # marker for domain boundary 
   z = 1.       # unit charge 
-  ec = 1. 	# electric constant [e]
+  ec = 8.854187817e-12 # electric constant [C^2/(Jm)]
+  M_TO_ANG = 1e-10
+  J_TO_KCAL = 0.000239005736
+  ec = ec / (M_TO_ANG * J_TO_KCAL) # ec [C^2/kcal A]
   epsilonExterior = 80. # dielectric constant in exterior []
-  kappa = 1/10.  # Inverse dybye length [1/A]
+  kappa = 1/10.  # Inverse dybye length [1/A] (should be determined by ionic strengths directly) 
   center = np.zeros(dim)      
-  kT = 0.69	# energy [kcal/mol]
+  kT = 0.59	# energy [kcal/mol]
   beta = 1/kT
 
   # ion
   ionC = 1. # ion conc [M]
-  ionRad=1. # ion radius [A]
+  ionRad=2. # ion radius [A]
 
   # modes
   mode = "linear"# linear, nonlinear, finitesize 
+  mode = "nonlinear"
+  mode = "finite"    
 
 
 class molecularBoundary(SubDomain):
@@ -59,6 +70,33 @@ class domainBoundary(SubDomain):
     result = result and on_boundary
     return result      
 
+# exterior domain 
+class OmegaOutside(SubDomain):
+    def inside(self, x, on_boundary):
+      result = np.linalg.norm(x-params.center) > (-params.eps+params.molRad)
+      #print "outside"
+      #print result 
+      return result 
+
+# interior domain 
+class OmegaInside(SubDomain):
+    def inside(self, x, on_boundary):
+      result = np.linalg.norm(x-params.center) <=(-params.eps+params.molRad)
+      #print x
+      #print params.center
+      #print "inside"
+      #print result 
+      return result 
+
+## special functions 
+def sinh(x=0):
+ print "Craptastic implementation. Need to use new version of fenics w sinh/Johans sympy model module"  
+ return (exp(x)-exp(-x))/2.
+
+def cosh(x=0):
+ print "Craptastic implementation. Need to use new version of fenics w sinh/Johans sympy model module"  
+ return (exp(x)+exp(-x))/2.
+
 
 # Create mesh and define function space
 def doit(filename):
@@ -71,6 +109,92 @@ def doit(filename):
     mesh = Mesh(filename)
 
   doPB(mesh)
+
+def doWholeDomainPB():
+  scale = 2*params.domRad 
+  ## mesh/functions 
+  square=1 
+  if square:
+    mesh = UnitSquare(100,100)
+    mesh.coordinates()[:] = scale * mesh.coordinates()
+  else:
+    # domain assigment doesn't seem to work correctly (fails on 'choose' funtion 
+    mesh = Mesh("sphere_2d_entire.xml") 
+
+  dim = 2
+  V = FunctionSpace(mesh, "DG", 0)
+  eps = Function(V)
+  kappa= Function(V)
+
+
+  ## point source 
+  params.center = scale*np.array([0.5,0.5])
+  print params.center
+  p = Point(params.center[0],params.center[1])  
+  #c = PointSource(V,p,magnitude=1) 
+  q1 = 1. # [e]
+  c = PointSource(V,p,q1)                 
+
+  ## boundary/domain conditions 
+  subdomains = MeshFunction('uint', mesh, dim)
+  subdomainBoundary = MeshFunction('uint', mesh, dim-1)
+  bcs = []
+  boundary = domainBoundary()
+  boundary.mark(subdomainBoundary,params.domMarker)
+  u0 = Constant(0.0)
+  bcs.append(DirichletBC(V, u0, subdomainBoundary,params.domMarker))
+
+  ## discontinuous epsilon 
+  # http://fenicsproject.org/documentation/tutorial/materials.html#working-with-two-subdomains
+  subdomain0 = OmegaOutside()
+  subdomain0.mark(subdomains, 0)
+  subdomain1 = OmegaInside()
+  subdomain1.mark(subdomains, 1)
+ 
+  eps_values = [params.epsilonExterior, 1.]  # values of k in the two subdomains
+  kappa_values = [params.epsilonExterior*params.kappa**2, 0.]  # values of k in the two subdomains
+  #for cell_no in range(len(subdomains.array())):
+  #  subdomain_no = subdomains.array()[cell_no]
+  # eps.vector()[cell_no] = eps_values[subdomain_no]
+  help = np.asarray(subdomains.array(), dtype=np.int32)
+  eps.vector()[:] = np.choose(help, eps_values)
+  kappa.vector()[:] = np.choose(help, kappa_values)
+
+
+  ## Define variational problem
+  u = TrialFunction(V)
+  v = TestFunction(V)
+  form = -1*eps*inner(grad(u), grad(v))*dx
+#  form += eps *v*dx
+
+  # eps*grad(u)grad(v) = kappa^2 uv
+  form += -1*kappa *u*v*dx
+
+  d= 0.1
+  factor = 4 * np.pi * params.ec*params.ec / (params.kT)
+  norm = 1/(d * np.sqrt(np.pi*2))
+  #form += Expression("factor*norm *exp(-( (x[0]-xC)*(x[0]-xC) + (x[1]-yC)*(x[1]-yC))/(2*d*d))",\
+  #                    xC=params.center[0],yC=params.center[1],d=d,norm=norm,factor=factor)*v*dx
+  A = lhs(form)
+  b = rhs(form)
+
+  # apply point charge 
+   # not correct 
+  #c.apply(b)
+  
+
+  x = Function(V)
+  solve(A==b, x, bcs)
+  # F = a
+  # problem = NonlinearVariationalProblem(F, x, bcs=bc, J=J)
+  # solver = NonlinearVariationalSolver(problem)
+  #solver.parameters["linear_solver"] = "gmres"
+  #solver.parameters["preconditioner"] = "ilu"
+  #solver.solve()
+
+
+  File("out.pvd") << x
+  
 
 def doPB(mesh):
   V = FunctionSpace(mesh, "Lagrange", 1)
@@ -103,26 +227,28 @@ def doPB(mesh):
   ## Define variational problem
   u = TrialFunction(V)
   v = TestFunction(V)
+
+  ## define form functions 
+  import ufl
+  namespace = ufl.__dict__.copy()
   
   # exterior
   # LHS  
-  form = params.epsilonExterior*inner(grad(u), grad(v))*dx
+  form = -1*params.epsilonExterior*inner(grad(u), grad(v))*dx
 
   # eps*grad(u)grad(v) = kappa^2 uv
   if(params.mode=="linear"):
-    form += -1*params.kappa*params.kappa *u*v*dx
+    form += -1*params.epsilonExterior*params.kappa*params.kappa *u*v*dx
   # eps*grad(u)grad(v) = kappa^2 sinh(u)*v
   elif(params.mode=="nonlinear"):
     print "Using nonlinear code"
-    def sinh(x=0):
-      print "Craptastic implementation. Need to use new version of fenics w sinh" 
-      print "Craptastic implementation. Need to Johan's sympy model module"           
-      return (exp(x)-exp(-x))/2.
-
-    import ufl
-    namespace = ufl.__dict__.copy()
     namespace["sinh"] = sinh
-    ufl_exp = eval(str(sinh()), namespace, {"x":u})
+    sinh_expr = eval(str(-1*params.epsilonExterior*params.kappa*params.kappa * sinh()), namespace, {"x":u})
+    print "sinh_expr %s" % sinh_expr
+
+    print "WARNING: not sure if ufl exp is reflected in the form (need to compare linear w non-linear"
+    form += sinh_expr*v*dx
+    print form 
 
     # Might need to cmopile this as UFL code, see pg 196 of dolfin manual
     # https://answers.launchpad.net/dolfin/+question/199782
@@ -133,14 +259,21 @@ def doPB(mesh):
   elif(params.mode=="finite"):
     print "WARNING: hyperbolic sine not recognized"
     arg = params.z*params.beta*params.ec*u
+
+    sinh_expr = eval(str(sinh()), namespace, {"x":arg})
+    print "sinh_expr %s" % sinh_expr
+    cosh_expr = eval(str(cosh()), namespace, {"x":arg})
+    print "cosh_expr %s" % cosh_expr
+
     a= params.ionRad
     phi0 = 2*a*a*a*params.ionC     
     rhs1 = 8 *np.pi * params.z*params.ec*params.ionC
     rhs1 *= 1/params.epsilonExterior
-    num = np.sinh(arg) 
-    denom = (1-phi0+phi0*np.cosh(arg))
+    num = sinh_expr      
+    denom = (1-phi0+phi0*cosh_expr)      
     rhs1 *= num/denom
     form += -1*rhs1*v*dx
+    print form 
   else:
     raise RuntimeError("What did you just tell me to do???")
   
@@ -183,7 +316,12 @@ if __name__ == "__main__":
   for i,arg in enumerate(sys.argv):
     if(arg=="-nonlinear"):
       params.mode="nonlinear"
+    if(arg=="-wholedom"):
+      doWholeDomainPB()
+      quit()
 
+
+   
 
 
 
