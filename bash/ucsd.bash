4a868ad8a8f12040c49ad802bee227afab601b41
huskeypm <huskeypm@gmail.com>
2013-06-19 08:30:00 -0700

    adding directory for bash files for env. var

---
+++ bash/ucsd.bash
@@ -0,0 +1,10 @@
+unalias python
+unalias ipython
+export MYPATH=/net//home/huskeypm/sources/mypython/
+export PYTHONPATH=$PYTHONPATH:$MYPATH/lib/python2.7/site-packages/
+
+source /net/home/huskeypm/sources/share/dolfin/dolfin.conf
+export BOOST_DIR=/usr
+export PYTHONPATH=/net/home/huskeypm/localTemp/srcs/smolfin/:$PYTHONPATH
+export PYTHONPATH=/net/home/huskeypm/localTemp/srcs/homogenization/:$PYTHONPATH
+
