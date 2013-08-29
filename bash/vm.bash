ee51bc78c91558e57cb6ebc55cf85890045db3d9
huskeypm <huskeypm@gmail.com>
2013-08-29 08:14:25 -0700

    Added gamer environ var

--- bash/vm.bash
+++ bash/vm.bash
@@ -3,3 +3,8 @@ export PYTHONPATH=$PYTHONPATH:~/sources/smolfin/
 export PYTHONPATH=$PYTHONPATH:~/sources/homogenization/
 export PYTHONPATH=$PYTHONPATH:~/sources/modified-pb/example/
 
+
+export SRCDIR=/home/huskeypm/sources/
+export VER=2.7
+export PREFIX=$SRCDIR/gamer-src/
+export PYTHONPATH=$PYTHONPATH:$PREFIX/lib/python$VER/site-packages
